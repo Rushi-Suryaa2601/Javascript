dfcf4e40a35eb5d9e0f3491813f724cdcf7218ce
Rushikesh Suryagandh <rushisuryagandh4455@gmail.com>
2023-11-27 15:31:54 +0530

    function

---
+++ function.js
@@ -0,0 +1,28 @@
+function nice(name) {
+    console.log("Hey " + name +" You are nice!")
+    console.log("Hey " + name +" Just looking like a wow!")
+   
+}
+nice("Rushi")
+nice("Rohan")
+
+function sum(a,b) {
+    // console.log(a+b)
+    return a+b
+    
+}
+// sum(3,5)
+result=sum(3,5)
+console.log(result)
+
+//Arrow Function
+const func1=(x)=>{
+    console.log("I am an arrow function",x)
+}
+func1(34);
+func1(66);
+func1(80);
+
+
+
+
