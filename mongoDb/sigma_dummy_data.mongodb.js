7b2731f1e044ca26766fb2cc406abe50f9281a26
Rushikesh Suryagandh <rushisuryagandh4455@gmail.com>
2024-01-20 15:08:37 +0530

    mongo db database

---
+++ mongoDb/sigma_dummy_data.mongodb.js
@@ -0,0 +1,65 @@
+
+
+// Select the database to use.
+use('sigma');
+
+// Insert a data into the courses collection.
+db.getCollection('courses').insertMany([
+  {
+    "name": "Mongo_DB",
+    "price": 20000,
+    "instructor": "Rushikesh"
+  },
+  {
+    "name": "Mongo_DB",
+    "price": 20000,
+    "instructor": "Rushikesh"
+  },
+  {
+    "name": "Mongo_DB",
+    "price": 20000,
+    "instructor": "Rushikesh"
+  },
+  {
+    "name": "Mongo_DB",
+    "price": 20000,
+    "instructor": "Rushikesh"
+  },
+  {
+    "name": "Mongo_DB",
+    "price": 20000,
+    "instructor": "Rushikesh"
+  },
+  {
+    "name": "Mongo_DB",
+    "price": 20000,
+    "instructor": "Rushikesh"
+  },
+  {
+    "name": "Mongo_DB",
+    "price": 20000,
+    "instructor": "Rushikesh"
+  },
+  {
+    "name": "Mongo_DB",
+    "price": 20000,
+    "instructor": "Rushikesh"
+  },
+  {
+    "name": "Mongo_DB",
+    "price": 20000,
+    "instructor": "Rushikesh"
+  },
+  {
+    "name": "Mongo_DB",
+    "price": 20000,
+    "instructor": "Rushikesh"
+  }
+]
+);
+
+
+// Print a message to the output window.
+console.log("Data Entered Successfully");
+
+
